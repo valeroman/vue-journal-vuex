42df1472677c4c50b4e35f1767e9cae459561468
Roman Valero <valeroman@gmail.com>
2021-12-06 14:13:36 -0500

    Configuracion OptionsAPI y Vuex

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,25 +1,53 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
   >
     <!-- titulo -->
     <div class="entry-title d-flex">
-        <span class="text-success fs-5 fw-bold">02</span>
-        <span class="mx-1 fs-5">Diciembre</span>
-        <span class="mx-2 fw-light">2021, jueves</span>
+        <span class="text-success fs-5 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-5">{{ month }}</span>
+        <span class="mx-2 fw-light">{{ yearDay }}</span>
     </div>
 
     <!-- descripcion -->
     <div class="entry-description">
-        Commodo fugiat culpa dolore incididunt sunt esse pariatur consectetur consectetur fugiat esse voluptate exercitation. Sunt aliqua veniam cillum aliquip. Duis velit incididunt amet enim. Consequat cillum non amet excepteur dolore. Lorem incididunt nulla minim magna proident ea veniam do exercitation excepteur. Sunt veniam enim aliqua voluptate sit. Sit esse ad fugiat sit fugiat labore laboris proident.
+        {{ shortText }}
     </div>
   </div>
 </template>
 
 <script>
-export default {
 
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+
+export default {
+  props: {
+    entry: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    shortText() {
+      return (this.entry.text.length > 130)
+        ? this.entry.text.substring(0, 130) + '...'
+        : this.entry.text
+    },
+    day() {
+      const date = new Date(this.entry.date)
+      return date.getDate()
+    },
+    month() {
+      const date = new Date(this.entry.date)
+      return months[date.getMonth()]
+    },
+    yearDay() {
+      const date = new Date(this.entry.date)
+      return `${ date.getFullYear() }, ${ days[date.getDay()] }`
+    }
+  }
 }
 </script>
 
--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -13,7 +13,12 @@ export default {
             path: ':id',
             name: 'entry',
             component: () =>
-                import ( /* webpackChunkName: "daybook-entry" */ '@/modules/daybook/views/EntryView.vue')
+                import ( /* webpackChunkName: "daybook-entry" */ '@/modules/daybook/views/EntryView.vue'),
+            props: (route) => {
+                return {
+                    id: route.params.id
+                }
+            }
         }
     ]
 }
